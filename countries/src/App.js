6bd060e97445e809490098bf8e97e2fbcc7710a8
gemasegarra <gema.segb@gmail.com>
2020-03-01 21:00:48 +0100

    create Global Style

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import { createGlobalStyle }  from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Nunito Sans', Sans-serif;
+    background-color: hsl(0, 0%, 98%);
+  }
+`
+
 function App() {
   return (
-   <div>Nothing here yet</div>
+    <>
+    <GlobalStyle></GlobalStyle>
   );
 }
 
