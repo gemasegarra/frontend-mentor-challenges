ef58fa89f33fc8f0a64ff986d30b213ee2edbf98
gemasegarra <gema.segb@gmail.com>
2020-03-01 21:01:28 +0100

    create footer

---
+++ countries/src/Components/Layout/Footer.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+import { FooterBar } from '../Styled-Components/Layout';
+
+function Footer() {
+  return (
+    <FooterBar>
+      <p>
+      Coded by
+        <a href="https://github.com/gemasegarra"> Gema S.</a> | 2020
+      </p>
+    </FooterBar>
+  );
+}
+
+export default Footer;
