d577dbdf62de24424ab351b0bdffff2aee7a2237
gemasegarra <gema.segb@gmail.com>
2020-03-01 21:01:39 +0100

    create header

---
+++ countries/src/Components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+import { HeaderBar } from '../Styled-Components/Layout';
+
+function Header () {
+  return (
+    <HeaderBar>
+      <p>Where in the world?</p>
+    </HeaderBar>
+  );
+}
+
+export default Header;
